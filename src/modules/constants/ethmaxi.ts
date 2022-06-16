56e91441e8278b383d2017fd761a880cede89ea2
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-16 20:48:29 +0530

    ft: ethmaxi and pmusdc deployed addresses

--- src/modules/constants/ethmaxi.ts
+++ src/modules/constants/ethmaxi.ts
@@ -1,10 +1,47 @@
 const wantTokenL1 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const keeperAddress = "0x55FE002aefF02F77364de339a1292923A15844B8";
-const governanceAddress = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
+const keeper = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
+const governance = "0x6b29610D6c6a9E47812bE40F1335918bd63321bf";
 
-const convexTEAddresses = {
+const convexTEInternal = {
   LP_TOKEN: "0x06325440d014e39736583c165c2963ba99faf14e",
   CONVEX_BASE_REWARD_POOL: "0x0A760466E1B4621579a82a39CB56Dda2F4E70f03",
 };
 
-export {wantTokenL1, keeperAddress, governanceAddress, convexTEAddresses};
+const vault = "0xAa0508FcD0352B206F558b2B817dcC1F0cc3F401";
+const batcher = "0x47c84A87A2a972769cc5DeDa28118617E3A48F8C";
+const harvester = "0xB79F25E8A6a3864937bAEB82bc3aaD16b4F57Dc7";
+const convexTradeExecutor = "0xbC7e39c8BA6BFf3E7FF7a893e0f3497696A9E977";
+const lyraETHOptionMarket = "0x1f6D98638Eee9f689684767C3021230Dd68df419";
+const lyraETHOptionToken = "0xfB1951b7EeF8E7613D3b09424fB4aEf805c16267";
+const lyraGlobals = "0x3856c594D35A24f7435731F9F5d7835887f0B94b";
+const sUSD = "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9";
+const wethL2 = "0x4200000000000000000000000000000000000006";
+const movrRegistry = "0xc30141B657f4216252dc59Af2e7CdB9D8792e1B0";
+const whale = "0xEB89FD3aadf7056C73BEA7A347f96C68404eEeA7";
+const ethOptionMarketView = "0x43592bffCF14f1e0A096091E125f023B2ccC2525";
+const wantTokenL2 = "0x4200000000000000000000000000000000000006";
+const optimismL1CrossDomainMessenger =
+  "0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1";
+const lyraTradeExecutorL1 = "0x0000000000000000000000000000000000000000";
+
+export {
+  wantTokenL1,
+  keeper,
+  governance,
+  convexTEInternal,
+  vault,
+  batcher,
+  harvester,
+  convexTradeExecutor,
+  lyraETHOptionMarket,
+  lyraETHOptionToken,
+  lyraGlobals,
+  ethOptionMarketView,
+  sUSD,
+  lyraTradeExecutorL1,
+  movrRegistry,
+  optimismL1CrossDomainMessenger,
+  wantTokenL2,
+  wethL2,
+  whale,
+};
--- src/modules/constants/pmusdc.ts
+++ src/modules/constants/pmusdc.ts
@@ -1,10 +1,63 @@
-export const wantTokenL1 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-export const keeperAddress = "0x55FE002aefF02F77364de339a1292923A15844B8";
-export const governanceAddress = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
-
-export const ConvexTEAddresses = {
+const convexTEInternal = {
   LP_TOKEN: "0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B",
   CONVEX_BASE_REWARD_POOL: "0xB900EF131301B307dB5eFcbed9DBb50A3e209B2e",
   CONVEX_BOOSTER: "0xF403C135812408BFbE8713b5A23a04b3D48AAE31",
   FRAX_POOL: "0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B",
+};
+
+const wantTokenL1 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const wantTokenL2 = "0x7F5c764cBc14f9669B88837ca1490cCa17c31607";
+const ust3Pool = "0xCEAF7747579696A2F0bb206a14210e3c9e6fB269";
+const curve3PoolZap = "0xA79828DF1850E8a3A3064576f380D90aECDD3359";
+const baseRewardPool = "0x7e2b9B5244bcFa5108A76D5E7b507CFD5581AD4A";
+const convexBooster = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
+const keeper = "0x55FE002aefF02F77364de339a1292923A15844B8";
+const governance = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
+
+const optimismL1CrossDomainMessenger =
+  "0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1";
+
+const perpVault = "0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60";
+const clearingHouse = "0x82ac2CE43e33683c58BE4cDc40975E73aA50f459";
+const clearingHouseConfig = "0xA4c817a425D3443BAf610CA614c8B11688a288Fb";
+const accountBalance = "0xA7f3FC32043757039d5e13d790EE43edBcBa8b7c";
+const orderBook = "0xDfcaEBe8f6ea5E022BeFAFaE8c6Cdae8D4E1094b";
+const exchange = "0xBd7a3B7DbEb096F0B832Cf467B94b091f30C34ec";
+const baseToken = "0x8c835dfaa34e2ae61775e80ee29e2c724c6ae2bb";
+const quoteTokenvUSDC = "0xC84Da6c8ec7A57cD10B939E79eaF9d2D17834E04";
+const movrRegistry = "0xc30141B657f4216252dc59Af2e7CdB9D8792e1B0";
+
+const vault = "0x1C4ceb52ab54a35F9d03FcC156a7c57F965e081e";
+const batcher = "0x9eBBccCcBfe01047dab8Ef07fAc3e4dBaD444dAC";
+const harvest = "0xF1D339D9456BC1e09b548E7946A78D9C4b5f1B68";
+const convexTradeExecutor = "0x3167b932336b029bBFE1964E435889FA8e595738";
+const perpTradeExecutor = "0x675A5c853fc2bc81E0eB79FC45e395d01Bd5D72D";
+const perpPositionHandlerL2 = "0x00fFC95A1A63dbd48A1E7397a3B051eA9E3F5Be9";
+
+export {
+  accountBalance,
+  baseRewardPool,
+  baseToken,
+  batcher,
+  clearingHouse,
+  clearingHouseConfig,
+  convexBooster,
+  convexTEInternal,
+  convexTradeExecutor,
+  curve3PoolZap,
+  exchange,
+  governance,
+  harvest,
+  keeper,
+  movrRegistry,
+  optimismL1CrossDomainMessenger,
+  orderBook,
+  perpPositionHandlerL2,
+  perpTradeExecutor,
+  perpVault,
+  quoteTokenvUSDC,
+  ust3Pool,
+  vault,
+  wantTokenL1,
+  wantTokenL2,
 };
