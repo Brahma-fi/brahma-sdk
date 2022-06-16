594aa6ebcbb0f5af72f6fb70c6ee5f6c9ac328a9
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-16 21:53:17 +0530

    ft: minor fix

--- src/modules/random.ts
+++ src/modules/random.ts
@@ -1,7 +1,5 @@
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {BigNumber, Wallet} from "ethers";
 import {hEthers} from "../utils";
-import {getSigner} from "./signer";
 
 export async function randomWallet(): Promise<Wallet> {
   const privKey = hEthers.utils.randomBytes(32);
