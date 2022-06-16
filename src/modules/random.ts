3f506c9d7089272bf89c4a015510c24bb05865ad
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-16 22:03:30 +0530

    rf: minor change

--- src/modules/random.ts
+++ src/modules/random.ts
@@ -1,21 +1,21 @@
 import {BigNumber, Wallet} from "ethers";
 import {hEthers} from "../utils";
 
-export async function randomWallet(): Promise<Wallet> {
+export const randomWallet = async (): Promise<Wallet> => {
   const privKey = hEthers.utils.randomBytes(32);
   const wallet = new hEthers.Wallet(privKey);
 
   return wallet;
-}
+};
 
-export function randomBN(
+export const randomBN = (
   upper: BigNumber,
   lower: BigNumber = BigNumber.from(0),
-): BigNumber {
+): BigNumber => {
   const out = lower
     .add(BigNumber.from(hEthers.utils.randomBytes(32)))
     .mod(upper);
 
   if (out.gt(0)) return out;
   return randomBN(upper);
-}
+};
