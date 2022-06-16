c60cb9af512f0b41a89e0533cc7290cdf0137bb0
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-16 19:47:35 +0530

    ft: get ethers injected hre instance

---
+++ src/utils/signer.ts
@@ -0,0 +1,8 @@
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {BigNumber, Wallet} from "ethers";
+
+// To get hre instance with ethers injected
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+const hre = require("hardhat");
