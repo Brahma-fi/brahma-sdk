38b94d38f9023538719f51361c1c090cb1bf4c5e
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-16 20:39:06 +0530

    ft: address constants for products

---
+++ src/modules/constants/ethmaxi.ts
@@ -0,0 +1,10 @@
+const wantTokenL1 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const keeperAddress = "0x55FE002aefF02F77364de339a1292923A15844B8";
+const governanceAddress = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
+
+const convexTEAddresses = {
+  LP_TOKEN: "0x06325440d014e39736583c165c2963ba99faf14e",
+  CONVEX_BASE_REWARD_POOL: "0x0A760466E1B4621579a82a39CB56Dda2F4E70f03",
+};
+
+export {wantTokenL1, keeperAddress, governanceAddress, convexTEAddresses};
